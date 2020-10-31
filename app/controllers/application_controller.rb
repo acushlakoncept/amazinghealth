8d10f439a3299dec17a7145e0c31d5524813f878
Uduak Essien <acushla4real@gmail.com>
2020-10-31 12:21:18 +0100

    update signup and sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   protect_from_forgery with: :exception
+  layout :layout_by_resource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :masquerade_user!
@@ -11,5 +12,14 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    end
+
+
+    def layout_by_resource
+      if devise_controller? && resource_name == :user && action_name == 'new'
+        'account'
+      else
+        'application'
+      end
     end
 end
