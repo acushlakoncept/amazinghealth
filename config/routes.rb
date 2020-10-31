6c655824dbb42c31c7e94621c621f2eb793ccdd5
Uduak Essien <acushla4real@gmail.com>
2020-10-31 10:00:01 +0100

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
